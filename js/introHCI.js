286d85bb13fc9e85e4950f48b812997d4375d3d0
Rahul Ramath <rahulramath1@gmail.com>
2016-02-03 17:46:56 -0800

    moved folders

---
+++ js/introHCI.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(submitClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	//$(this).css("background-color","#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+    $(".project-description").toggle();
+    $(".img").toggle();
+}
+
+function submitClick(e) {
+	var projectName = $("#project").val();
+	$(projectName).animate({
+		width: $("#width").val()
+	});
+
+	var newText = $('#description').val();
+	$(projectName + " .project-description").text(newText);
+}
+
+$(function() {
+    var $document   = $(document),
+        $inputRange = $('input[type="range"]');
+    
+    // Example functionality to demonstrate a value feedback
+    function valueOutput(element) {
+        var value = element.value,
+            output = element.parentNode.getElementsByTagName('output')[0];
+        output.innerHTML = value;
+    }
+    for (var i = $inputRange.length - 1; i >= 0; i--) {
+        valueOutput($inputRange[i]);
+    };
+    $document.on('input', 'input[type="range"]', function(e) {
+        valueOutput(e.target);
+    });
+    // end
+  
+    $inputRange.rangeslider({
+      polyfill: false 
+    });
+});
