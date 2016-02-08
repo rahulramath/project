7ce24e64a8c55208b3d82794def16bf717f8a4cc
Austin Chinn <achinn@ucsd.edu>
2016-02-07 23:20:02 -0800

    round 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-//var add = require('./routes/add');
+var homepage = require('./routes/homepage');
 // Example route
 // var user = require('./routes/user');
 
